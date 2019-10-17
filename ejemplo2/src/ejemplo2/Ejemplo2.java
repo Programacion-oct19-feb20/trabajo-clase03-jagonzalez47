db22b6ef78be31eb6448fb80cba31500be1a05b0
jagonzalez <jagonzalez47@utpl.edu.ec>
2019-10-17 12:47:51 -0500

    ejemplos02

--- ejemplo2/src/ejemplo2/Ejemplo2.java
+++ ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -16,6 +16,18 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombres = "Jandry Alcivar";
+                System.out.println(nombres);
+                String apellidos = "Gonzalez Palta";
+                        System.out.println(apellidos);
+                        String ciudad = "Loja";
+                        System.out.println(ciudad);
+                        String edad = "19";
+                                System.out.println(edad);
+                                String institucion = "universidad tecnica particular de loja";
+                                        System.out.println(institucion);
+                                        
+                                        
     }
     
 }
